214503cf99f619f477be331ba191625fa1031f1a
Shado2107 <pd39almeida@outlook.com>
2022-03-30 15:16:57 +0000

    api controllers and routes

--- api/server/routes/router.js
+++ api/server/routes/router.js
@@ -3,6 +3,7 @@ const app = express();
 const route = express.Router();
 
 const services= require('../services/render');
+const controller = require('../controller/controller');
 
 /**
  * @description Root Route
@@ -24,5 +25,13 @@ app.get('/add_user', services.add_user)
  */
 
 app.get('/update_user', services.update_user)
+
+
+//API
+route.post('/api/users',controller.create);
+route.get('/api/users',controller.find);
+route.put('/api/users',controller.update);
+route.delete('/api/users',controller.delete);
+
 
 module.exports = route
