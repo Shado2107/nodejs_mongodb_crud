dc2df59ca4b1affdba073f4f25f840ea2ae63302
Shado2107 <pd39almeida@outlook.com>
2022-04-25 13:22:20 +0000

    update

--- api/server/services/render.js
+++ api/server/services/render.js
@@ -17,6 +17,12 @@ exports.add_user = (req, res) => {
     res.render('add_user')
 }
 
-exports.update_user = (req, res) => {
-    res.render('update_user')
-}   
+exports.update_user = (req, res) =>{
+    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
+        .then(function(userdata){
+            res.render("update_user", { user : userdata.data})
+        })
+        .catch(err =>{
+            res.send(err);
+        })
+}
